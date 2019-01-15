edfbec9e2744f0da1a16216317d41a90ef33fc3c
张少康 <zhangshaokang@zhangshaokangMacBook-Pro.local>
2019-01-15 11:29:51 +0800

    create youAndMe

--- lib/viewcontrollers/mine.dart
+++ lib/viewcontrollers/mine.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:job_find_pro/viewcontrollers/youAndMe.dart';
 
 class MineVC extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return CupertinoPageScaffold(
       child: Center(
-        child: Text('MineVC'),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                CupertinoPageRoute(builder: (context) => YouAndMeVC()));
+          },
+          child: Text('MineVC'),
+        ),
       ),
     );
   }
---
+++ lib/viewcontrollers/youAndMe.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class YouAndMeVC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: Text('data'),
+      ),
+    );
+  }
+}
